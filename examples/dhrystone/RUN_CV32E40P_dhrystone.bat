a8c59d43322de413ac9f33860e1ad31846213c10
duncangraham-Imperas <graham@imperas.com>
2023-04-27 12:29:58 +0100

    update v20230425

---
+++ examples/dhrystone/RUN_CV32E40P_dhrystone.bat
@@ -0,0 +1,13 @@
+@echo off
+
+;rem move into the Example Directory
+set BATCHDIR=%~dp0%
+cd /d %BATCHDIR%
+
+..\..\bin\Windows64\riscvOVPsimCOREV.exe ^
+    --program dhrystone.RISCV32.elf ^
+    --variant CV32E40P ^
+    --processorname CVE4P ^
+    %*
+
+if not defined calledscript ( pause )
---
+++ examples/fibonacci/RUN_CV32E40P_fibonacci.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd $(dirname $0)
+bindir=$(dirname $(dirname $(pwd)))/bin/Linux64
+
+${bindir}/riscvOVPsimCOREV.exe \
+    --program fibonacci.RISCV32.elf \
+    --variant CV32E40P \
+    --processorname CVE4P \
+    "$@"
